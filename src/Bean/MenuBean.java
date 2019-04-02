af21d707d60f3766772bffca2137b71bec894ec7
Soonyub Hwang <nowonbun@naver.com>
2019-04-03 07:31:57 +0900

    20190403 morning

--- src/Bean/MenuBean.java
+++ src/Bean/MenuBean.java
@@ -1,11 +1,16 @@
 package Bean;
 
+import java.util.List;
+
 import Common.IF.MenuType;
 
 public class MenuBean {
 	private MenuType type;
 	private String name;
 	private String url;
+	private MenuBean parent;
+	private List<MenuBean> child;
+	private String id;
 
 	public MenuType getType() {
 		return type;
@@ -29,6 +34,30 @@ public class MenuBean {
 
 	public void setUrl(String url) {
 		this.url = url;
+	}
+
+	public List<MenuBean> getChild() {
+		return child;
+	}
+
+	public void setChild(List<MenuBean> child) {
+		this.child = child;
+	}
+
+	public MenuBean getParent() {
+		return parent;
+	}
+
+	public void setParent(MenuBean parent) {
+		this.parent = parent;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
 	}
 
 }
--- src/BlogApi/MenuBuilder.java
+++ src/BlogApi/MenuBuilder.java
@@ -2,11 +2,15 @@ package BlogApi;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import Bean.MenuBean;
 import Common.FactoryDao;
+import Common.Util;
 import Common.IF.MenuType;
+import Dao.BlogDao;
 import Dao.CategoryDao;
+import Model.Blog;
 import Model.Category;
 
 public class MenuBuilder {
@@ -22,26 +26,49 @@ public class MenuBuilder {
 	private MenuBuilder() {
 
 	}
-	
+
 	private List<MenuBean> menulist = null;
 
 	public List<MenuBean> getMenu() {
-		if(menulist == null) {
+		if (menulist == null) {
 			return init();
 		}
 		return menulist;
 	}
 
 	public List<MenuBean> init() {
-		List<Category> list = FactoryDao.getDao(CategoryDao.class).selectAll();
-		menulist = new ArrayList<>();
-		for (Category item : list) {
+		List<Blog> list = FactoryDao.getDao(BlogDao.class).selectAll();
+		if (menulist == null) {
+			menulist = new ArrayList<>();
+		} else {
+			menulist.clear();
+		}
+		for (Blog blog : list) {
 			MenuBean bean = new MenuBean();
-			bean.setType(MenuType.sub);
-			bean.setName(item.getName());
-			bean.setUrl(null);
+			bean.setType(MenuType.parent);
+			bean.setName(blog.getName());
+			bean.setUrl(blog.getUrl());
+			List<Category> categoryList = blog.getCategories();
+			for (Category category : blog.getCategories()) {
+				if (bean.getChild() == null) {
+					bean.setChild(new ArrayList<>());
+				}
+				MenuBean child = new MenuBean();
+				if (category.getParent() == null) {
+					child.setParent(bean);
+				} else {
+					Optional<MenuBean> search = menulist.stream().filter(x -> Util.StringEquals(x.getId(), category.getCategoryId())).findFirst();
+					if (search.isPresent()) {
+						child.setParent(search.get());
+					} else {
+						child.setParent(bean);
+					}
+				}
+
+			}
 			menulist.add(bean);
 		}
+
 		return menulist;
 	}
 }
